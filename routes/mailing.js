bb2a53eb2bff0a93e171442d10decb38ac5aa75e
aikidoesurf81 <amoctezuma81@gmail.com>
2025-02-02 07:50:45 -0800

    award tokens sendggrid route

--- routes/mailing.js
+++ routes/mailing.js
@@ -66,181 +66,26 @@ router.post('/requestInfo', function (req, res, next) {
     
 });
 
-/*
-router.post('/verifyEmail', function (req, res, next) {
-    
-    var code = common.generateVerificationCode();
-
-    var mailObj = {
-        to: req.body.email,
-        from: "info@lifebalancetool.com",
-        templateId: "d-74df876039834d0e936f7eff7d575b93",
-        dynamic_template_data: {
-            email: req.body.email,
-            verification_code: code
-        }
-    }
-
-    sendMail(mailObj);
-    res.status(200).send({});
-
-    verify.storeEmailcode(req.body.user_id, code);
-    
-});
-*/
-
-/*
-router.post('/thankYouForPurchase', function (req, res, next) {
-    
-    var mailObj = {
-        to: req.body.email,
-        from: "info@lifebalancetool.com",
-        templateId: "d-211ca56ca95e46e88e03599f1cda9653",
-        dynamic_template_data: {
-            email: req.body.email,
-            tier: req.body.tier,
-            billing_period: req.body.billing_period,
-            number_clients: req.body.number_clients
-        }
-    }
-
-    sendMail(mailObj);
-    res.status(200).send({});
-
-});
-*/
-
-/*
-router.post('/yourPlanHasChanged', function (req, res, next) {
-    
-    var mailObj = {
-        to: req.body.email,
-        from: "info@lifebalancetool.com",
-        templateId: "d-f996397603a941819402c7cab4f8bfb1",
-        dynamic_template_data: {
-            email: req.body.email,
-            verification_code: code
-        }
-    }
-
-    sendMail(mailObj);
-    res.status(200).send({});
-
-});
-*/
-/*
-router.post('/newSignup', function (req, res, next) {  
+router.post('/tokensAwarded', function (req, res, next) {
     //console.log('sendgrid/passwordReset', req.body);
 
     var mailObj = {
-        to: [
-            {
-                email:"info@lifebalancetool.com"
-            },
-            {
-                email:"amoctezuma81@gmail.com"
-            },
-            {
-                email:"sarah.anne808@gmail.com"
-            }
-        ],
-        from: "info@lifebalancetool.com",
-        templateId: "d-dbe66383303946369be4f99aaf95564f",
-        dynamic_template_data: {
-            name: req.body.name,
-            email: req.body.email,
-            companyname: req.body.companyname
-        }
-    }
-
-    sendMail(mailObj);
-    res.status(200).send({});
-    
-});
-*/
-/*
-router.post('/newSubscription', function (req, res, next) {
-    //console.log('sendgrid/passwordReset', req.body);
-
-    var mailObj = {
-        to: [
-            {
-                email:"info@lifebalancetool.com"
-            },
-            {
-                email:"amoctezuma81@gmail.com"
-            },
-            {
-                email:"sarah.anne808@gmail.com"
-            }
-        ],
-        from: "info@lifebalancetool.com",
-        templateId: "d-9f16ff0b97ee450badbe08228963fb80",
-        dynamic_template_data: {
-            name: req.body.name,
-            email: req.body.email,
-            companyname: req.body.companyname
-        }
-    }
-
-    sendMail(mailObj);
-    res.status(200).send({});
-    
-});
-*/
-/*
-router.post('/newMessage', function (req, res, next) {
-    //console.log('sendgrid/passwordReset', req.body);
-
-    var mailObj = {
-        to: [
-            {
-                email:"info@lifebalancetool.com"
-            },
-            {
-                email:"amoctezuma81@gmail.com"
-            },
-            {
-                email:"sarah.anne808@gmail.com"
-            }
-        ],
-        from: "info@lifebalancetool.com",
-        templateId: "d-87e120befbf148cdad5ebce2e5e2f66d",
-        dynamic_template_data: {
-            name: req.body.name,
-            email: req.body.email,
-            message: req.body.message
-        }
-    }
-
-    sendMail(mailObj);
-    res.status(200).send({});
-    
-});
-*/
-/*
-router.post('/verifyEmail', function (req, res, next) {
-    
-    var code = common.generateVerificationCode();
-
-    var mailObj = {
         to: req.body.email,
-        from: "info@lifebalancetool.com",
-        templateId: "d-c28b40f0367140a8bb101ef2daa5bb62",
+        from: "admin@sportzbattle.com",
+        templateId: "d-ceaa15217fbd46b0a7510d1951a971ae",
         dynamic_template_data: {
-            email: req.body.email,
-            verification_code: code
+            username: req.body.username,
+            number_tokens: req.body.number_tokens,
+            message_number_one: req.body.message_number == 1 ? true:false,
+            message_number_two: req.body.message_number == 2 ? true:false,
+            message_number_three: req.body.message_number == 3 ? true:false
         }
     }
 
     sendMail(mailObj);
     res.status(200).send({});
-
-    //store code in database
-    //verify.storeEmailcode(req.body.user_id, code);
     
 });
-*/
 
 function sendMail(request){
 
@@ -249,8 +94,8 @@ function sendMail(request){
     sgMail.send(request).then(() => {
         console.log('Message sent')
     }).catch((error) => {
-        //console.log(error.response.body)
-        console.log(error.response.body.errors[0].message)
+        console.log(error)
+        //console.log(error.response.body.errors[0].message)
     })
     
 }
